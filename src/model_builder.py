fae91908f329334ebf477ca55921634071c8ac1c
Bernando Virto Gunawan <bernandogunawan@gmail.com>
2026-01-02 21:57:34 +0700

    change efficientnet_b0 to b2

--- src/model_builder.py
+++ src/model_builder.py
@@ -3,9 +3,9 @@ import torchvision.models as models
 import torch.nn as nn
 
 def build_model(model_choice, num_classes, pretrained=True):
-    if model_choice == "efficientnet_b0":
-        weight = models.EfficientNet_B0_Weights.DEFAULT
-        model = models.efficientnet_b0(weights=weight)
+    if model_choice == "efficientnet_b2":
+        weight = models.EfficientNet_B2_Weights.DEFAULT
+        model = models.efficientnet_b2(weights=weight)
         model.classifier[1] = nn.Linear(
             model.classifier[1].in_features, num_classes
         )
