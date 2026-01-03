51b38ce3f46d210d14e496ed00eb44e4061fd779
Bernando Virto Gunawan <bernandogunawan@gmail.com>
2026-01-03 15:34:06 +0700

    change experiment workflow to notebook

--- src/utils.py
+++ src/utils.py
@@ -2,20 +2,12 @@
 Contains various utility functions for PyTorch model training and saving.
 """
 import torch
-from pathlib import Path
 
-def save_model(model, target_dir, model_name):
-
-  # Create target directory
-  target_dir_path = Path(target_dir)
-  target_dir_path.mkdir(parents=True,
-                        exist_ok=True)
+def save_model(model, model_name):
 
   # Create model save path
   assert model_name.endswith(".pth") or model_name.endswith(".pt")
-  model_save_path = target_dir_path / model_name
 
   # Save the model state_dict()
-  print(f"[INFO] Saving model to: {model_save_path}")
-  torch.save(obj=model.state_dict(),
-             f=model_save_path)
+  print(f"[INFO] Saving model")
+  torch.save(obj=model.state_dict(), model_name)
