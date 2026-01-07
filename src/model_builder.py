83bb66042fa9ddd9d8f5081d5fbb7f8b9d12cadb
Bernando Virto Gunawan <bernandogunawan@gmail.com>
2026-01-07 11:17:06 +0700

    update model_builder.py

--- src/model_builder.py
+++ src/model_builder.py
@@ -7,14 +7,24 @@ def build_model(model_choice, num_classes, pretrained=True):
         weight = models.EfficientNet_B2_Weights.DEFAULT
         model = models.efficientnet_b2(weights=weight)
         model.classifier[1] = nn.Linear(
+            nn.Dropout(p=0.2, inplace=True),
             model.classifier[1].in_features, num_classes
         )
+
+        for param in model.parameters():
+            param.requires_grad = False
+        for param in model.classifier.parameters():
+            param.requires_grad = True
 
     elif model_choice == "resnet50":
         weight = models.ResNet50_Weights.DEFAULT
         model = models.resnet50(weights=weight)
         model.fc = nn.Linear(model.fc.in_features, num_classes)
 
+        for param in model.parameters():
+            param.requires_grad = False
+        for param in model.fc.parameters():
+            param.requires_grad = True
     else:
         raise ValueError("Unsupported model choice")
 
